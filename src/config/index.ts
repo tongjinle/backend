e6468052b1bd49e44b66383984a79153376d885a
九条研介 <dinopresent@qq.com>
2019-05-22 17:28:29 +0800

    默认环境为test

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ import testConf from "./test";
 import devConf from "./dev";
 import prodConf from "./prod";
 
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || "test";
 console.log("node env:", env);
 
 let conf: IConfig = {};
