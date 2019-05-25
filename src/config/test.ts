6270c41d02891dae7e060c85acf94b384915e64b
tongjinle <1328192478@qq.com>
2019-05-25 17:11:44 +0800

    ~

--- src/config/test.ts
+++ src/config/test.ts
@@ -2,7 +2,8 @@ import IConfig from "./iconfig";
 const config: IConfig = {
   dbName: "testCute",
   mockToken: "sannian.zst",
-  mockOpenId: "sannian.zst"
+  mockOpenId: "sannian.zst",
+  port: 7003
 };
 
 export default config;
