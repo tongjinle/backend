00d960637a1b3f6f1e56b0ebd0d6c2b74e5e8e9a
九条研介 <dinopresent@qq.com>
2019-06-05 21:30:34 +0800

    ~

--- src/service/noticeService.ts
+++ src/service/noticeService.ts
@@ -1,15 +1,36 @@
 // 官方通知
 import getMongoClient from "../getMongoClient";
 import config from "../config";
+import { isMainThread } from "worker_threads";
+import md5 = require("md5");
 
 let dbName: string = config.dbName;
 
 type Notice = {
   id: string;
+  token: string;
   text: string;
   // 奖励
   coin?: number;
 };
+
+export async function add(
+  token: string,
+  text: string,
+  coin: number = 0
+): Promise<void> {
+  let mongo = await getMongoClient();
+  await mongo
+    .db(dbName)
+    .collection("notice")
+    .insertOne({
+      id: md5(Math.random().toString()),
+      token,
+      text,
+      coin,
+      isRead: false
+    });
+}
 
 export async function list(token: string): Promise<Notice[]> {
   let rst: Notice[];
@@ -24,6 +45,7 @@ export async function list(token: string): Promise<Notice[]> {
   rst = data.map(n => {
     return {
       id: n.id,
+      token: n.token,
       text: n.text,
       coin: n.coin || 0
     };
@@ -32,13 +54,13 @@ export async function list(token: string): Promise<Notice[]> {
   return rst;
 }
 
-export async function read(token: string, id: string): Promise<void> {
+export async function read(id: string): Promise<void> {
   let mongo = await getMongoClient();
   await mongo
     .db(dbName)
     .collection("notice")
     .updateOne(
-      { token, id },
+      { id, isRead: false },
       {
         $set: {
           isRead: true,
--- src/test/simple/noticeService.test.ts
+++ src/test/simple/noticeService.test.ts
@@ -1,11 +1,52 @@
 import assert = require("assert");
 import * as noticeService from "../../service/noticeService";
+import { MongoClient } from "mongodb";
+import getMongoClient from "../../getMongoClient";
+import config from "../../config";
+import { beforeEach } from "mocha";
+
+let dbName = config.dbName;
 
 describe("notice service", () => {
-  befor;
-  it("list", async function() {
-    let;
+  let mongo: MongoClient;
+  before(async function() {
+    mongo = await getMongoClient();
   });
 
-  it("read", async function() {});
+  beforeEach(async function() {
+    await mongo
+      .db(dbName)
+      .collection("notice")
+      .deleteMany({});
+  });
+
+  after(async function() {
+    await mongo.close();
+  });
+
+  it("add", async function() {
+    await noticeService.add("tongjinle", "hi", 100);
+
+    let data = await mongo
+      .db(dbName)
+      .collection("notice")
+      .findOne({ token: "tongjinle" });
+
+    assert(data && data.isRead === false);
+  });
+
+  it("list && read", async function() {
+    await noticeService.add("tongjinle", "hi", 100);
+    await noticeService.add("tongjinle", "hi2", 50);
+    await noticeService.add("zhousuting", "hi3", 100);
+
+    let list = await noticeService.list("tongjinle");
+
+    assert(list.length === 2);
+
+    await noticeService.read(list[0].id);
+    let list2 = await noticeService.list("tongjinle");
+
+    assert(list2.length === 1);
+  });
 });
