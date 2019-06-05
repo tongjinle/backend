73a3749c942d630e67d2df4c24d6a8d436a8460a
九条研介 <dinopresent@qq.com>
2019-06-05 21:07:18 +0800

    ~

---
+++ src/test/simple/noticeService.test.ts
@@ -0,0 +1,11 @@
+import assert = require("assert");
+import * as noticeService from "../../service/noticeService";
+
+describe("notice service", () => {
+  befor;
+  it("list", async function() {
+    let;
+  });
+
+  it("read", async function() {});
+});
