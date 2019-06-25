8aafd0aa58ff21923c6992123fb406d292e0620a
tongjinle <1328192478@qq.com>
2019-06-25 18:12:38 +0800

    upvote

---
+++ src/service/upvote.ts
@@ -0,0 +1,35 @@
+import { getMongoClient } from "../getMongoClient";
+import config from "../config";
+
+// 是否已经点赞
+export async function isUpvoted(
+  diaryId: string,
+  userId: string
+): Promise<boolean> {
+  let rst: boolean;
+
+  let mongo = await getMongoClient();
+  rst = !!(await mongo
+    .db(config.dbName)
+    .collection("upvote")
+    .findOne({ diaryId, userId }));
+
+  return rst;
+}
+
+// 点赞
+export async function upvote(diaryId: string, userId: string): Promise<void> {
+  let mongo = await getMongoClient();
+
+  // upvote的记录
+  await mongo
+    .db(config.dbName)
+    .collection("upvote")
+    .insertOne({ diaryId, userId });
+
+  // 给日记加1
+  await mongo
+    .db(config.dbName)
+    .collection("diary")
+    .updateOne({ diaryId }, { $inc: { upvote: 1 } });
+}
