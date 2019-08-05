da6cb303e3e3c3fbd00dad69f6cf84dca1daae94
tongjinle <1328192478@qq.com>
2019-08-05 16:13:18 +0800

    recover

--- src/routes/testHandle.ts
+++ src/routes/testHandle.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-import { getMongoClient } from "../getMongoClient";
 import config from "../config";
+import { getCollection, getMongoClient } from "../getMongoClient";
 
 export default function handle(app: express.Express) {
   // 测试
@@ -28,97 +28,36 @@ export default function handle(app: express.Express) {
     res.end("db test:" + data.ts);
   });
 
-  app.get("/test/recover/", async (req, res) => {
-    if (process.env.NODE_ENV !== "test") {
+  app.get("/test/recover", async (req, res) => {
+    if (process.env.NODE_ENV !== "dev") {
       res.end("only in TEST enviroment");
       return;
     }
 
-    console.log(config.connectStr, config.dbName);
-
-    let client = await getMongoClient();
-    let users = client.db(config.dbName).collection("user");
-    let photos = client.db(config.dbName).collection("photo");
+    let users = await getCollection("user");
 
     await users.deleteMany({});
-    await photos.deleteMany({});
 
     await users.insertMany([
-      { userId: "tongyan", nickname: "童颜" },
-      { userId: "qianqian", nickname: "浅浅" }
-    ]);
-
-    await photos.insertMany([
       {
-        id: "1",
-        score: 100,
-        url:
+        userId: "tongyan",
+        nickname: "童颜",
+        gender: "female",
+        logoUrl:
           "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E7%AB%A5%E9%A2%9C/080942a0fee28fa6bbc1be6790718757.jpg",
-        nickname: "童颜",
-        userId: "tongyan",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E7%AB%A5%E9%A2%9C/080942a0fee28fa6bbc1be6790718757.jpg"
+        city: "上海",
+        birthYear: 2000,
+        coin: 90
       },
       {
-        id: "2",
-        score: 90,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E7%AB%A5%E9%A2%9C/19a560688838c9696d08b4016ac65906.jpg",
-        nickname: "童颜",
-        userId: "tongyan",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E7%AB%A5%E9%A2%9C/19a560688838c9696d08b4016ac65906.jpg"
-      },
-      {
-        id: "3",
-        score: 70,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E6%B5%85%E6%B5%85/0bafca06466c9de2c366ed21f10d8b44.jpg",
-        nickname: "浅浅",
         userId: "qianqian",
+        nickname: "浅浅",
+        gender: "female",
         logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E6%B5%85%E6%B5%85/0bafca06466c9de2c366ed21f10d8b44.jpg"
-      },
-
-      {
-        id: "4",
-        score: 70,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/169d8540e196aac24c6b51a3083acae8.jpg",
-        nickname: "小鹿猪比",
-        userId: "xiaoluzhubi",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/169d8540e196aac24c6b51a3083acae8.jpg"
-      },
-      {
-        id: "5",
-        score: 90,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/278a0f74b1ff35f9b761d0f67bcb6a24.jpg",
-        nickname: "小鹿猪比",
-        userId: "xiaoluzhubi",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/278a0f74b1ff35f9b761d0f67bcb6a24.jpg"
-      },
-      {
-        id: "6",
-        score: 80,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/034b8d45c2140f1a4697cf1d9c4b4a34.jpg",
-        nickname: "小鹿猪比",
-        userId: "xiaoluzhubi",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/034b8d45c2140f1a4697cf1d9c4b4a34.jpg"
-      },
-      {
-        id: "7",
-        score: 40,
-        url:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/169d8540e196aac24c6b51a3083acae8.jpg",
-        nickname: "小鹿猪比",
-        userId: "xiaoluzhubi",
-        logoUrl:
-          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E5%B0%8F%E9%B9%BF%E8%B1%AC%E6%AF%94/169d8540e196aac24c6b51a3083acae8.jpg"
+          "https://mucheng2020.oss-cn-hangzhou.aliyuncs.com/pacong/twitter/%E6%B5%85%E6%B5%85/0bafca06466c9de2c366ed21f10d8b44.jpg",
+        city: "北京",
+        birthYear: 2001,
+        coin: 100
       }
     ]);
 
